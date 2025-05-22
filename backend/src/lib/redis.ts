d3666bf93ae20cbb252068c045c32454ef8477ca
Akie <legaspiernstdanielle@gmail.com>
2025-05-22 21:19:27 +0800

    feature: added redis

---
+++ backend/src/lib/redis.ts
@@ -0,0 +1,22 @@
+import { createClient, type RedisClientType } from "redis"
+
+let client: RedisClientType | null = null
+
+export const initClient = async () => {
+	if(!client) {
+		try {
+			const tempClient: RedisClientType = createClient()
+
+			tempClient.on("connect", () => console.log("Connected"))
+			tempClient.on("error", e => console.log("Error connecting: " + e))
+
+			await tempClient.connect()
+			client = tempClient
+		} catch (e) {
+			console.error("Failed to connect Redis client:", e)
+			throw e
+		}
+	}
+
+	return client
+}
